0ca854573d6f6418e8caefc8515d09088febbd12
Connor Lane <connor.lane858@gmail.com>
2023-07-09 22:48:05 -0400

    Reformulate loss functions
    - Don't use negative examples from the same sample
    - Rescale wiring cost to average over samples and columns

--- no_sharing/loss.py
+++ no_sharing/loss.py
@@ -36,9 +36,9 @@ class LocalInfoNCELoss(nn.Module):
     def forward(self, embedding: torch.Tensor):
         # TODO: Should we detach the positive or negative examples?
         # TODO: Do we need a memory bank? DDP gather?
-
         N, L, C = embedding.shape
         assert L == self.height**2
+        assert N >= 8, "insufficient batch size"
 
         device = embedding.device
         embedding = F.normalize(embedding, dim=-1)
@@ -47,25 +47,31 @@ class LocalInfoNCELoss(nn.Module):
         # its neighbors.
         # (N, L, C)
         target = torch.matmul(self.weight, embedding)
-        # (N, L, 1)
-        sim_pos = torch.sum(embedding * target, dim=2, keepdim=True)
+        # (N, L)
+        sim_pos = torch.sum(embedding * target, dim=-1) / self.temperature
 
-        # Negative similarity: Random sample of embeddings from the full batch.
+        # Negative similarity: Random sample of embeddings from other images in the batch.
         # Should hopefully prevent collapse.
-        flattened = embedding.view(-1, C)
+        # TODO: for some reason the embedding is not contiguous. Figure out why.
+        flattened = embedding.reshape(-1, C)
         neg_indices = random_sample(
-            size=N * L, num_samples=self.num_negative, device=device
+            size=N * L, max_samples=self.num_negative, device=device
         )
         # (num_neg, C)
         negatives = flattened[neg_indices]
         # (N, L, num_neg)
-        sim_neg = torch.matmul(embedding, negatives.t())
+        sim_neg = torch.matmul(embedding, negatives.t()) / self.temperature
 
-        logits = torch.cat([sim_pos, sim_neg], dim=2)
-        logits = logits.view(N * L, 1 + self.num_negative)
-        target = torch.zeros(N * L, dtype=torch.int64, device=device)
+        # Mask of negative examples from other samples in the batch
+        # (N, 1, num_neg)
+        batch_idx = neg_indices // L
+        neg_mask = torch.arange(N, device=device)[:, None, None] != batch_idx
 
-        loss = F.cross_entropy(logits, target)
+        # Offset of numerical stability
+        offset = sim_pos.detach().amax()
+        pos = torch.exp(sim_pos - offset)
+        neg = torch.sum(torch.exp(sim_neg - offset) * neg_mask, dim=-1)
+        loss = -torch.mean(torch.log(pos / (pos + neg)))
         return loss
 
     def extra_repr(self) -> str:
@@ -80,30 +86,25 @@ class WiringCost(nn.Module):
     An L1 penalty weighted by the wiring distance on a grid.
     """
 
-    def __init__(
-        self,
-        height: int,
-        lambd: float = 1.0,
-        squared: bool = False,
-    ):
+    def __init__(self, height: int, lambd: float = 1.0):
         super().__init__()
         self.height = height
         self.lambd = lambd
-        self.squared = squared
 
         # euclidean distance weights, shape (height^2, height^2)
         dist = distance_weights(height)
-        # normalize to a unit box
-        dist = dist / height
-        if squared:
-            dist = dist.square()
         self.dist: torch.Tensor
         self.register_buffer("dist", dist)
 
     def forward(self, weight: torch.Tensor) -> torch.Tensor:
-        assert weight.shape[-2:] == (self.height**2, self.height**2)
-        loss = self.lambd * torch.sum(weight.abs() * self.dist)
-        return loss
+        L = self.height**2
+        assert weight.shape[-2:] == (L, L)
+
+        # TODO: should the cost just increase linearly with wiring distance like this?
+        cost = weight.abs() * self.dist
+        cost = cost.sum(dim=(-2, -1))
+        cost = (self.lambd / L) * cost.mean()
+        return cost
 
     def extra_repr(self) -> str:
-        return f"height={self.height}, lambd={self.lambd}, squared={self.squared}"
+        return f"height={self.height}, lambd={self.lambd}"
